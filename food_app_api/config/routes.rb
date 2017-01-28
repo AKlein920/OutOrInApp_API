65c6c6d6bb8cc04387dc15f9206bee60450236a7
Abi Auslander <abi920@gmail.com>
2017-01-28 08:56:01 -0500

    abandoned devise; attempting to get users to work with json web tokens

--- food_app_api/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  resources :places
-  resources :recipes
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
