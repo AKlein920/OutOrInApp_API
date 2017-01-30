6d0b41f45285447777a82841471b63e4bd834491
Abi Auslander <abi920@gmail.com>
2017-01-30 17:38:48 -0500

    reconfigured app to have one to many relationship with users and recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   resources :users do
+    resources :recipes
     collection do
       post '/login', to: 'users#login'
     end
-  end
-
-  resources :categories do
-    resources :recipes
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
